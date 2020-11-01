3852b2fd03fd3a182b738bdafe79fc7ef43ae463
Valentinaga1 <1881@holbertonschool.com>
2020-11-01 15:08:02 +0000

    Updating base class and adding new clasess

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+    Represents amenitty class  that inherits from BaseModel
+'''
+from models.base_model import BaseModel
+
+class Amenity (BaseModel):
+	'''
+        Repretesents Class Amenity with public class attribute
+    '''
+	name = ""
